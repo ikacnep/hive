2d2934a65d9bb7211d0135c67dcdb890bb2e56d5
Ivan Kaspersky <Ivan.Kaspersky@infowatch.com>
2017-11-04 13:25:12 +0300

    веб-сервер умеет отвечать на добавление и движение фигур

--- server/server.py
+++ server/server.py
@@ -5,39 +5,120 @@ import flask
 app = flask.Flask(__name__)
 
 
+class IncorrectMove(Exception):
+    def __init__(self, message):
+        super(IncorrectMove, self).__init__(message)
+
+
+@app.errorhandler(IncorrectMove)
+def handle_incorrect_move(error):
+    response = flask.jsonify({"error_message": str(error)})
+    response.status_code = 400
+    return response
+
+
+class Hive:
+    board = []
+    state = {
+            'current_turn': 'white',
+            'is_opening_move': True,
+            'white': {
+                'available_figures': {
+                    'ant': 3,
+                    'spider': 2,
+                    'beetle': 2,
+                    'grasshopper': 3,
+                    'queen': 1,
+                }
+            },
+            'black': {
+                'available_figures': {
+                    'ant': 3,
+                    'spider': 2,
+                    'beetle': 2,
+                    'grasshopper': 3,
+                    'queen': 1,
+                }
+            }
+    }
+
+
 @app.route('/')
 def hello_world():
     return flask.render_template('index.html')
 
 
-@app.route('/action/click')
-def click():
+@app.route('/action/add', methods=['POST'])
+def add():
+    data = flask.request.json
+
+    board = app.hive.board
+    state = app.hive.state
+
+    pieces_at_these_coordinates = [piece for piece in board if piece['coordinates'][0:1] == data['coordinates']]
+
+    if pieces_at_these_coordinates:
+        raise IncorrectMove("Nope, can't place atop of hive")
+
+    available_figures = state[state['current_turn']]['available_figures']
+
+    if not available_figures[data['figure']]:
+        raise IncorrectMove("You don't have any more %s to place" % data['figure'])
+
+    available_figures[data['figure']] = available_figures[data['figure']] - 1
+
+    if len(board) > 1:
+        state['is_opening_move'] = False
+
+    piece = {
+            'player': state['current_turn'],
+            'id': len(board),
+            'figure': data['figure'],
+            'coordinates': data['coordinates'] + [0]
+    }
+
+    board.append(piece)
+    board.sort(key=lambda piece: piece['coordinates'][2])
+    
+    state['current_turn'] = 'white' if state['current_turn'] == 'black' else 'black'
+
     return flask.jsonify(
-        ok=True
+            piece=piece,
+            state=state
+    )
+
+
+@app.route('/action/move', methods=['POST'])
+def move():
+    data = flask.request.json
+
+    board = app.hive.board
+    state = app.hive.state
+
+    piece = next(piece for piece in app.hive.board if piece['id'] == data['id'])
+
+    piece['coordinates'] = data['coordinates'] + [0]
+
+    pieces_at_these_coordinates = [piece for piece in app.hive.board if piece['coordinates'][0:1] == data['coordinates']]
+
+    if pieces_at_these_coordinates:
+        piece['coordinates'][2] = pieces_at_these_coordinates[0][2] + 1
+
+    app.hive.board.sort(key=lambda piece: piece['coordinates'][2])
+
+    state['current_turn'] = 'white' if state['current_turn'] == 'black' else 'black'
+
+    return flask.jsonify(
+            piece=piece,
+            state=state
     )
 
 
 @app.route('/board')
 def get_board():
-    board = [
-        dict(
-            coordinates=(2, 0, 0),
-            piece='ant',
-            player='white'
-        ),
-        dict(
-            coordinates=(2, 1, 0),
-            piece='queen',
-            player='black'
-        ),
-        dict(
-            coordinates=(3, 0, 0),
-            piece='queen',
-            player='white'
-        ),
-    ]
+    return flask.jsonify(board=app.hive.board, state=app.hive.state)
 
-    return flask.jsonify(
-        board=board,
-        state={'color': 'white', 'your_turn': True, 'can_move': True, 'placed_queen': True}
-    )
+
+if __name__ == '__main__':
+    app.hive = Hive()
+    app.run('0.0.0.0', debug=True)
--- server/static/main.js
+++ server/static/main.js
@@ -23,16 +23,27 @@ const BORDER = {
     black: '#000',
     nobody: '#FFF',
 
+    placed: 'magenta',
     selected: 'green',
+    moved: 'yellow',
     can_move_here: 'blue',
     cant_move_here: 'red'
 };
 
+if (window.console && console.log) {
+    window.log = function() {
+        console.log.apply(console.log, arguments);
+    }
+}
 
 jQuery(function ($) {
+    log('initializing');
+
     var canvas = $('canvas')[0].getContext('2d'),
         b = 50,
         d = 20;
+
+    var game = 'not loaded yet';
 
     var sqrt3 = Math.sqrt(3);
 
@@ -40,23 +51,30 @@ jQuery(function ($) {
     var g = sqrt3 / 2 * b;
 
     function DrawHex(options) {
+        log('DrawHex:', options);
+
+        if (!options.player) {
+            var piece_on_board = FindPieceAt(options.coordinates);
+            options.player = piece_on_board && piece_on_board.player;
+        }
+
         var hex_color = HEX_COLOR[options.player || 'nobody'];
         var text_color = TEXT_COLOR[options.player || 'nobody'];
-        var border_color = BORDER[options.border || options.player || 'nobody'];
+        var border_color = options.border || BORDER[options.player || 'nobody'];
 
-        console.log('DrawHex', options, '->', hex_color, text_color, border_color);
+        log('DrawHex', options, '->', hex_color, text_color, border_color);
 
-        var r = options.r;
-        var q = options.q;
+        var r = options.coordinates[0];
+        var q = options.coordinates[1];
 
         var x = g * (1 + r + 2 * q);
         var y = b * (1 + 1.5 * r);
 
-        if (hex_color !== border_color) {
-            DoDrawHex(x, y, b, g, border_color);
-        }
+        DoDrawHex(x, y, b, g, border_color);
 
-        DoDrawHex(x, y, a, sqrt3 / 2 * a, hex_color);
+        if (hex_color !== border_color) {
+            DoDrawHex(x, y, a, sqrt3 / 2 * a, hex_color);
+        }
 
         var font_size = b / 2 - d / 4;
 
@@ -65,12 +83,14 @@ jQuery(function ($) {
         canvas.textBaseline = 'middle';
         canvas.font = font_size + 'px sans-serif';
 
-        var text = (options.text || '') + ' (' + (r) + ', ' + (q) + ')';
+        var text = options.text || '';
 
         canvas.fillText(text, x, y);
     }
 
     function DoDrawHex(x, y, b, g, fill_style) {
+        log('DoDrawHex', [].slice.apply(arguments));
+
         canvas.beginPath();
         canvas.fillStyle = fill_style;
         canvas.strokeStyle = fill_style;
@@ -85,7 +105,46 @@ jQuery(function ($) {
         canvas.fill();
     }
 
+    function DrawPiece(piece, border) {
+        log('DrawPiece', [].slice.apply(arguments));
+       
+        DrawHex({
+            coordinates: piece.coordinates,
+            player: piece.player,
+            text: piece.figure[0] + ':' + piece.id + '/' + (piece.coordinates[2] || 0),
+            border: border
+        });
+    }
+
+    function SortBoard() {
+        log('SortBoard', [].slice.apply(arguments));
+        
+        game.board.sort(function(a, b) {
+            return a.coordinates[2] - b.coordinates[2];
+        });
+    }
+
+    function DrawBoard() {
+        log('DrawBoard', [].slice.apply(arguments));
+
+        for (var piece of game.board) {
+            DrawPiece(piece);
+        }
+    }
+
+    function FindPieceAt(coordinates) {
+        log('FindPieceAt', [].slice.apply(arguments));
+
+        for (var piece of game.board) {
+            if (piece.coordinates[0] == coordinates[0] && piece.coordinates[1] == coordinates[1]) {
+                return piece;
+            }
+        }
+    }
+
     function Post(url, data) {
+        log('Post', [].slice.apply(arguments));
+        
         return $.ajax(url, {
             data: JSON.stringify(data),
             contentType: 'application/json',
@@ -94,47 +153,92 @@ jQuery(function ($) {
     }
 
     function OnHexClick(r, q) {
-        console.log('Click at', r, q);
+        log('OnHexClick', [].slice.apply(arguments));
 
-        /*
-        var adding_piece = $('#add').val();
+        var adding_figure = $('#add').val();
 
-        if (adding_piece) {
-            window.selected_hex = null;
-
+        if (adding_figure) {
             Post('/action/add', {
-                piece: adding_piece,
-                hex: {
-                    r: r,
-                    q: q
-                }
+                figure: adding_figure,
+                coordinates: [r, q]
             })
-                .done(function() {
+                .done(function(data) {
+                    game.state = data.state;
 
+                    if (game.last_highlighted_piece) {
+                        DrawPiece(game.last_highlighted_piece);
+                    }
+
+                    game.board.push(data.piece);
+                    SortBoard();
+
+                    DrawPiece(data.piece, BORDER.placed);
+
+                    game.last_highlighted_piece = data.piece;
                 });
         } else {
-            if (window.selected_hex) {
-                if (window.selected_hex.r === r && window.selected_hex.q === q) {
-                    window.selected_hex = null;
+            if (game.selected_piece) {
+                var coordinates = game.selected_piece.coordinates;
+
+                if (coordinates[0] === r && coordinates[1] === q) {
+                    DrawPiece(game.selected_piece);
+                    game.selected_piece = null;
+
+                    if (game.last_highlighted_piece) {
+                        DrawPiece(game.last_highlighted_piece);
+                    }
+                } else {
+                    Post('/action/move', {
+                        id: game.selected_piece.id,
+                        coordinates: [r, q]
+                    })
+                        .done(function(data) {
+                            game.state = data.state;
+
+                            game.board = game.board.filter(function(piece) { return piece.id != data.piece.id; } );
+                            game.board.push(data.piece);
+
+                            SortBoard();
+
+                            DrawPiece(data.piece, BORDER.moved);
+                            DrawHex({coordinates: game.selected_piece.coordinates});
+
+                            if (game.last_highlighted_piece) {
+                                DrawPiece(game.last_highlighted_piece);
+                            }
+
+                            game.selected_piece = null;
+                            game.last_highlighted_piece = data.piece;
+                        });
                 }
             } else {
-                window.selected_hex = {r: r, q: q};
+                game.selected_piece = FindPieceAt([r, q]);
 
-                DrawHex(r, q, 'black', 'green');
+                if (game.selected_piece) {
+                    DrawPiece(game.selected_piece, BORDER.selected);
+                }
             }
         }
-
-        window.last_selected_hex = {r: r, q: q};
-        */
     }
 
-    $.getJSON('/board').done(function(data) {
-        console.log(data);
+    $(document).ajaxError(function(event, jqxhr, settings, error) {
+        console.log('Ajax error', arguments);
 
-        for (var piece of data.board) {
-            DrawHex({r: piece.coordinates[0], q: piece.coordinates[1], player: piece.player, text: piece.piece[0]});
+        var error_message = 'Well, fuck.';
+
+        if (jqxhr.responseJSON) {
+            error_message = jqxhr.responseJSON.error_message;
         }
+
+        alert(error_message);
     });
+
+    $.getJSON('/board')
+        .done(function(state) {
+            game = state;
+            SortBoard()
+            DrawBoard(state.board);
+        });
 
     $('#canvas').click(function (event) {
         var offset = $(this).offset();
@@ -152,14 +256,5 @@ jQuery(function ($) {
         var q = Math.floor((m + l) / 3);
 
         OnHexClick(r, q);
-
-        /*
-        canvas.beginPath();
-        canvas.fillStyle = 'blue';
-        canvas.arc(x, y, 3, 0, 7);
-        canvas.fill();
-        */
     });
-
-    window.last_selected_hex = null;
 });
