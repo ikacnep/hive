afc72349c892bbadfcdf8cc3e2bed86e64d86628
Ivan <ikacnep@mail.ru>
2018-03-07 08:33:03 +0300

    Нет токена — нет бота

--- run.py
+++ run.py
@@ -7,14 +7,6 @@ import signal
 from server import meat, telegram_bot
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
-bot = telegram_bot.HiveTelegramBot(open('conf/telegram.bot.token').read().strip())
-
-
 def stop_and_quit(*unused_args):
     # Фласк не даёт нормального интерфейса для остановки, а бот не останавливается
     os._exit(1)
@@ -23,6 +15,23 @@ def stop_and_quit(*unused_args):
 signal.signal(signal.SIGTERM, stop_and_quit)
 signal.signal(signal.SIGINT, stop_and_quit)
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger('hive.launcher')
+logger.info('Starting up')
+
+try:
+    telegram_token = open('conf/telegram.bot.token').read().strip()
+except:
+    logger.exception('Cannot read bot token, not launching bot')
+else:
+    logger.debug('Launching bot')
+    telegram_bot.HiveTelegramBot(telegram_token)
+
+logger.debug('Launching meat')
 
 meat.start(
     tls_cert='conf/fullchain.pem',
